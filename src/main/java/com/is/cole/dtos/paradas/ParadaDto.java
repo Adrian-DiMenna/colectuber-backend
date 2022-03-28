40ae27322aa53285cf920f7d9ccc89c97cbd1ff7
Di Menna <85318236+Adrian-DiMenna@users.noreply.github.com>
2022-03-28 10:47:09 -0400

    CLE-29-Paradas services
    Realice los servicios para los controladores

---
+++ src/main/java/com/is/cole/dtos/paradas/ParadaDto.java
@@ -0,0 +1,70 @@
+package com.is.cole.dtos.paradas;
+
+import com.is.cole.dtos.BaseDto;
+
+public class ParadaDto extends BaseDto{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	private String nombre;
+	
+	private String descripcion;
+	
+	private String image;
+	
+	private double latitud;
+	
+	private double longitud;
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public double getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(double latitud) {
+		this.latitud = latitud;
+	}
+
+	public double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+	
+}
---
+++ src/main/java/com/is/cole/services/paradas/ParadaServiceImpl.java
@@ -0,0 +1,73 @@
+package com.is.cole.services.paradas;
+
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.is.cole.daos.IParadaDao;
+import com.is.cole.dtos.Result;
+import com.is.cole.dtos.paradas.ParadaDto;
+import com.is.cole.entities.Parada;
+
+import java.util.List;
+
+public class ParadaServiceImpl implements IParadaService {
+
+	@Autowired
+	private IParadaDao paradaDao;
+	
+	@Override
+	public ParadaDto saveParada(ParadaDto dto) {
+		Parada bean = parseDtotoBean(dto);
+		Parada beanGuardado = paradaDao.save(bean);
+		return parseBeanToDto(beanGuardado);
+	}
+
+	@Override
+	public Result<ParadaDto> getAllParadas() {
+		Result<ParadaDto> result = new Result<>();
+		List<ParadaDto> list = paradaDao.findAll().stream().map((bean)->{
+			return parseBeanToDto(bean);
+		}).collect(Collectors.toList());
+		result.setResult(list);
+		return result;
+	}
+
+	@Override
+	public ParadaDto getParadaById(Integer id) {
+		Parada beanObtenido = paradaDao.getById(id);
+		return parseBeanToDto(beanObtenido);
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		paradaDao.deleteById(id);
+	}
+	
+	private ParadaDto parseBeanToDto(Parada bean) {
+		ParadaDto dto = new ParadaDto();
+		
+		dto.setId(bean.getId());
+		dto.setNombre(bean.getNombre());
+		dto.setDescripcion(bean.getDescripcion());
+		dto.setImage(bean.getImagen());
+		dto.setLongitud(bean.getLongitud());
+		dto.setLatitud(bean.getLatitud());
+		
+		return dto;
+	}
+	
+	private Parada parseDtotoBean(ParadaDto dto) {
+		Parada bean = new Parada();
+		
+		bean.setId(dto.getId());
+		bean.setNombre(dto.getNombre());
+		bean.setDescripcion(dto.getDescripcion());
+		bean.setImagen(dto.getImage());
+		bean.setLongitud(dto.getLongitud());
+		bean.setLatitud(dto.getLatitud());
+		
+		return bean;
+	}
+
+}
