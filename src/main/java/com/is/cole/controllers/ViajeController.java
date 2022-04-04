a8e2ec8606b00296a02b60c264b986fc0aef46e8
DanielMatsuura <85190874+DanielMatsuura@users.noreply.github.com>
2022-04-04 09:51:40 -0400

    CLE-37 Backend servicios terminados

---
+++ src/main/java/com/is/cole/controllers/ViajeController.java
@@ -0,0 +1,5 @@
+package com.is.cole.controllers;
+
+public class ViajeController {
+
+}
--- src/main/java/com/is/cole/services/viajes/ViajesServiceImpl.java
+++ src/main/java/com/is/cole/services/viajes/ViajesServiceImpl.java
@@ -1,19 +1,15 @@
 package com.is.cole.services.viajes;
 
-import java.util.Date;
-
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.is.cole.daos.IColectivoDao;
 import com.is.cole.daos.IRecorridoDao;
-import com.is.cole.daos.IRoleDao;
-import com.is.cole.daos.IRolesUsuarioDao;
 import com.is.cole.daos.IUserDao;
 import com.is.cole.daos.IViajeDao;
 import com.is.cole.dtos.Result;
 import com.is.cole.dtos.Viajes.ViajeDto;
-import com.is.cole.entities.Roles;
 import com.is.cole.entities.Viaje;
 
 @Service
@@ -28,79 +24,55 @@ public class ViajesServiceImpl implements IViajesService {
 	@Autowired
 	private IRecorridoDao recorridoDao;
 	
-	// Estados
-	private final Integer PENDIENTE = 0;
-	private final Integer EN_CURSO = 1;
-	private final Integer INACTIVO = 2;
-	private final Integer FINALIZADO = 3;
-
-	// Umbrales de tiempo
-	private final Long UMBRAL_ACTIVO = (long) 100000;
-	private final Long UMBRAL_TERMINADO = (long) 2000000;
-
 	/********************** Normal CRUDs **********************/
 
 	@Override
 	public ViajeDto saveViaje(ViajeDto dto) {
-
-		return null;
+		Viaje beanGuardado= viajeDao.save(parseDtoToBeanViaje(dto));
+		return parseBeanToDtoViaje(beanGuardado);
 	}
 
 	@Override
 	public void deleteViaje(Integer id) {
-		// TODO Auto-generated method stub
-
+		viajeDao.deleteById(id);
 	}
 
 	@Override
 	public ViajeDto getViaje(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Viaje beanObtenido= viajeDao.getById(id);
+		return parseBeanToDtoViaje(beanObtenido);
 	}
 
 	@Override
 	public Result<ViajeDto> getAllViaje() {
-		// TODO Auto-generated method stub
-		return null;
+		Result<ViajeDto> resultViaje= new Result<>();
+		
+		List<ViajeDto> dtosObtenido = (viajeDao.findAll()
+				.stream().map( bean-> parseBeanToDtoViaje(bean)).collect(Collectors.toList()));
+		resultViaje.setResult(dtosObtenido);
+		
+		return resultViaje;
 	}
 
 	/********************** Special functions **********************/
 
 	@Override
 	public ViajeDto getByChoferIdViaje(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Viaje beanObtenido = viajeDao.findByUsuarioId(id);
+		return parseBeanToDtoViaje(beanObtenido);
 	}
-
-	/********************** Specific functions **********************/
 
 	/********************** Parses **********************/
 
+	//No esta seteado el state todavia
+	
 	private ViajeDto parseBeanToDtoViaje(Viaje bean) {
 		ViajeDto dto = new ViajeDto();
+		dto.setId(bean.getId());
 		dto.setChofer_id(bean.getUsuario().getId());
 		dto.setColectivo_id(bean.getColectivo().getId());
 		dto.setDestino(bean.getDestino());
 		dto.setRecorrido_id(bean.getRecorrido().getId());
-
-		// set state
-		Long last_update = bean.getLast_update();
-		Long this_time = new Date().getTime();
-
-		if (bean.getTerminado() == true) {
-			dto.setEstado(FINALIZADO);
-		} else if (last_update == null) {
-			dto.setEstado(PENDIENTE);
-		} else {
-			Long diff_time = this_time - last_update;
-			if (diff_time > UMBRAL_TERMINADO) {
-				dto.setEstado(FINALIZADO);
-			} else if (diff_time > 0 && diff_time < UMBRAL_ACTIVO) {
-				dto.setEstado(EN_CURSO);
-			} else {
-				dto.setEstado(INACTIVO);
-			}
-		}
 		return dto;
 	}
 	
@@ -111,15 +83,6 @@ public class ViajesServiceImpl implements IViajesService {
 		bean.setId(dto.getId());
 		bean.setRecorrido(recorridoDao.getById(dto.getRecorrido_id()));
 		bean.setUsuario(usuarioDao.getById(dto.getChofer_id()));
-		
-		//Estados
-		if(dto.getEstado()== FINALIZADO) {
-			bean.setTerminado(true);
-		}else {
-			bean.setTerminado(false);
-		}
-		bean.setLast_update(null);
-		
 		return bean;	
 	}
 }
